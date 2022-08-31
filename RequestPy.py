516bbcaa96eb3b9a4016fc2ae69bcb7fb4953a6d
AdrianKay <110188655+AdrianKay@users.noreply.github.com>
2022-08-31 12:58:07 -0400

    Update RequestPy.py

--- RequestPy.py
+++ RequestPy.py
@@ -40,23 +40,23 @@ r.text[0:100]
 
 # Use single quotation marks for defining string
 url='https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0101EN-SkillsNetwork/IDSNlogo.png'
+
 # We can make a get request:
-
 r=requests.get(url)
+
 # We can look at the response header:
-
 print(r.headers)
+
 # We can see the 'Content-Type'
-
 r.headers['Content-Type']
-# An image is a response object that contains the image as a bytes-like object. As a result, we must save it using a file object. First, we specify the file path and name
 
+# An image is a response object that contains the image as a bytes-like object. As a result, we must save it using a file object. First, we specify the file path and name
 path=os.path.join(os.getcwd(),'image.png')
 path
-# We save the file, in order to access the body of the response we use the attribute content then save it using the open function and write method:
 
+# We save the file, in order to access the body of the response we use the attribute content then save it using the open function and write method:
 with open(path,'wb') as f:
     f.write(r.content)
-# We can view the image:
 
+  # We can view the image:
 Image.open(path)  
